0a628d85ff17504611ee3b724dd90cbaeaa5bffc
lascor <kiefer@citrin.ch>
2016-06-23 12:55:32 +0200

    firstadd
    Firstadd to share

---
+++ README.md
@@ -0,0 +1,14 @@
+=Quizly=
+
+==Disclaimer==
+This Little Project is an Assingment for the Module 256: "Clientseitige Anwendung realisieren" directed by 
+
+==Function==
+This Project is a litte Quizapp where you can add/edit/delete your own Questions and Question Modules.
+Requirement is it is only written in javascript so no Persistance now :D
+
+
+==Paticipants==
+
+Oliver Widemann
+Patrick Jan Kiefer aka. "Lascor"
---
+++ overview.js
@@ -0,0 +1,95 @@
+// Code goes here
+
+var modulesContainer = document.getElementById('modules');
+
+//object modules has multiple Lernmodule
+//ein Lernmodul has multiple Fragen
+//eine Frage has multiple Antworten
+var modules = {
+  "Lernmodul1" : {
+    "Frage1" : {
+      "Antwort1": "a",
+      "Antwort2": "b"
+    },
+    "Frage2" : {
+      "Antwort1": "a",
+      "Antwort2": "b"
+    }
+  },
+  "Lernmodul2" : {
+    "frage1": "a"
+  }
+}
+
+function listModules() {
+  for (var index in modules) {
+    //List every Module by Name(key value in modules)
+    moduleName = document.createTextNode(index);
+    var moduleNameContainer = document.createElement('DIV');
+    moduleNameContainer.appendChild(moduleName);
+    
+    //create function Buttons on every Module
+    moduleNameContainer.appendChild(createButton("start",index));
+    moduleNameContainer.appendChild(createButton("edit",index));
+    moduleNameContainer.appendChild(createButton("delete",index));
+    
+    modulesContainer.appendChild(moduleNameContainer);
+  }
+
+}
+
+
+function addNewButton() {
+  var addButtonContainer = document.getElementById('addButton');
+  addButtonContainer.appendChild(createButton("add","1"));
+}
+
+//create a button with ID and create a EventListener on it
+function createButton(text,id) {
+  //create Button Elementand add text
+  var button = document.createElement('button');
+  var buttonText = document.createTextNode(text);
+  button.appendChild(buttonText);
+  //Add an ID just in Case
+  button.id = text + id;
+  //create an Eventlistener on Button
+  createEventlistener(button,text,id);
+  
+  return button; 
+}
+
+//Add Functionality to Buttons
+function createEventlistener(buttonAction,action,id) {
+  buttonAction.addEventListener("click", function() {buttonEvent(action,id)});
+}
+
+function deleteModule() {
+  
+}
+
+function buttonEvent (action,id) {
+  // Debug Stuff
+  console.log(action);
+  console.log(id);
+  
+  // Witch Action to execute on Call
+  switch(action) {
+    case "delete":
+        deleteModule();
+      break;
+    case "edit":
+        alert("Action edit " + id);
+      break;
+    case "add":
+        alert("Action Add Module");
+      break;
+    case "start":
+        alert("start Module");
+      break;
+    default:
+      "function not supported";
+  }
+}
+
+listModules();
+addNewButton();
